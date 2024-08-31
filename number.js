dc046417453a2300618e0635b16cfe10f67ab083
krithiga <onlyrithi@gmail.com>
2024-08-31 22:06:52 +0530

    add code html, css, js

--- number.js
+++ number.js
@@ -1,19 +1,22 @@
 // Selecting input box and paragraph 
 var guessnumber = document.getElementById("guessnumber")
-var result = document.getElementById("result")
+//var result = document.getElementById("result")
+var btn = document.getElementById("btn")
 var randomNumber = Math.floor(Math.random() * 10) + 1
-var Score = document.getElementById("Score")
+//var Score = document.getElementById("Score")
+const audio = document.querySelector("audio")
 var totalscore = 10
 function check() {
     var enterednumber = guessnumber.value
     if (randomNumber == enterednumber) {
         console.log("Right")
-        result.textContent = "Right"
-        alert("you Won")
+       // result.textContent = "Right"
+        confetti()
+        audio.play(); 
     }
     else {
-        console.log("Wrong")
-        result.textContent = "Wrong"
+        //console.log("Wrong")
+       // result.textContent = "Wrong"
         Score.textContent = "Score:" + totalscore
         totalscore = totalscore - 1
     }
